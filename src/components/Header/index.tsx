f8fda5fce3eef3e8fd63ecadf4059f56f1b30743
Lucas Barra <lucastbarra@hotmail.com>
2021-08-22 01:11:13 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
-
+import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.container}>
       <Image width={40} height={23} src="/images/logo.svg" alt="logo" />
+      <div className={styles.content}>
       <h2>spacetraveling</h2>
       <p>.</p>
+      </div>
     </header>
   );
 }
