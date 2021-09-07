124ed8d584f1fe89a41b1fdf516480519f2cde05
lucas barra <lucastbarra@hotmail.com>
2021-09-07 14:01:21 -0300

    date formated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import { getPrismicClient } from '../services/prismic';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 // import commonStyles from '../styles/common.module.scss';
 // import styles from './home.module.scss';
 
@@ -39,7 +42,7 @@ export default function Home({ postsPagination }: HomeProps) {
         <PostPreview title={post.data.title} subtitle={post.data.subtitle} author={post.data.author} date={post.first_publication_date} />
       ))}
 
-      
+
     </div>
   );
 }
@@ -60,7 +63,13 @@ export const getStaticProps = async () => {
   const results = postsResponse.results.map(post => {
     return {
       uid: post.uid,
-      first_publication_date: post.first_publication_date,
+      first_publication_date: format(
+        Date.parse(post.first_publication_date),
+        "dd MMM y",
+        {
+          locale: ptBR,
+        }
+      ),
       data: {
         title: RichText.asText(post.data.title),
         subtitle: RichText.asText(post.data.subtitle),
